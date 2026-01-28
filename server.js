46905512b80ad309022cf17d28fcb1238a7fbc36
Harrison Merrill <hwalkermerrill@gmail.com>
2026-01-28 16:01:16 +0900

    complete default setup instructions

---
+++ server.js
@@ -0,0 +1,17 @@
+// Imports
+import express from "express";
+
+// Constants
+const app = express();
+const name = process.env.NAME || "NAME NOT SET";
+const PORT = 3000;
+
+// Define a route handler for the root URL ('/')
+app.get("/", (req, res) => {
+  res.send(`Hello, ${name}!`);
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
