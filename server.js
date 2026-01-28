535ac82e59a3a75aae0515566d7d9766bae760c4
Harrison Merrill <hwalkermerrill@gmail.com>
2026-01-28 16:04:23 +0900

    add port to env file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from "express";
 // Constants
 const app = express();
 const name = process.env.NAME || "NAME NOT SET";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Define a route handler for the root URL ('/')
 app.get("/", (req, res) => {
