f757c350e6e43ab3155cd045cb031ff409846645
Harrison Merrill <hwalkermerrill@gmail.com>
2026-03-06 21:51:29 +0900

    setup login role data in the res

--- src/models/forms/login.js
+++ src/models/forms/login.js
@@ -4,13 +4,15 @@ import db from "../db.js";
 
 const findUserByEmail = async (email) => {
   const query = `
-    SELECT id,
-      name,
-      email,
-      password,
-      created_at
-    FROM users 
-    WHERE LOWER(email) = LOWER($1) LIMIT 1
+    SELECT u.id,
+      u.name,
+      u.email,
+      u.password,
+      u.created_at,
+      r.role_name AS "roleName"
+    FROM users AS u
+    INNER JOIN roles AS r ON u.role_id = r.id 
+    WHERE LOWER(u.email) = LOWER($1) LIMIT 1
   `;
 
   const result = await db.query(query, [email]);
