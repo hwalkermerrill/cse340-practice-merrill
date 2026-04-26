d31b1828b940a389dcabe83191387d370ca75012
Harrison Merrill <hwalkermerrill@gmail.com>
2026-04-26 18:52:19 +0900

    fix mistake

--- server.js
+++ server.js
@@ -27,9 +27,6 @@ app.set("views", path.join(__dirname, "src/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// test database connection on startup
-console.log("DB_URL at runtime:", process.env.DB_URL);
-
 // Session Configuration
 app.use(session({
 	store: new PgSession({
